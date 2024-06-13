b00461b8d7a666193d346bdf5c545e99d9570974
govin20 <govinjunizal20@gmail.com>
2024-06-13 19:28:50 +0700

    Update key

--- src/components/Produk/Produk.js
+++ src/components/Produk/Produk.js
@@ -4,14 +4,16 @@ import { initializeApp } from 'firebase/app';
 import { getFirestore, collection, addDoc, getDocs, updateDoc, doc, deleteDoc } from 'firebase/firestore';
 
 // Your web app's Firebase configuration
+require('dotenv').config();
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBlb4PYD9vUP1QdGcOMhRckeL9QFIywuP0',
-  authDomain: 'indofresh-fc226.firebaseapp.com',
-  projectId: 'indofresh-fc226',
-  storageBucket: 'indofresh-fc226.appspot.com',
-  messagingSenderId: '504050591085',
-  appId: '1:504050591085:web:8770251b68ee48aba5e664',
-  measurementId: 'G-679DBB4W89',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
